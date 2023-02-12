d714933f7737e5e94d6ad8ac17dc3335c2d3116c
Keskil Nikolaev <nik96keskil@yandex.ru>
2023-02-12 18:02:42 +0700

    email pattern fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { useEffect } from "react";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import { emailPattern } from "../../utils/constants";
 import Header from "../Header/Header";
 import "./Profile.css"
 
@@ -9,7 +10,16 @@ export default function Profile(props) {
   const [inputsDisabled, setInputDisabled] = useState(true);
   const [newName, setNewName] = useState("")
   const [newEmail, setNewEmail] = useState("")
+  const [isFormValid, setIsFormValid] = useState(false);
   const [profileSavedMessage, setProfileSavedMessage] = useState("")
+  const [isEmailError, setEmailError] = useState(false);
+  const [isNameError, setIsNameError] = useState(false)
+  // const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+
+  useEffect(() => {
+    setIsFormValid(isEmailError && isNameError);
+    console.log(isEmailError, isNameError)
+  }, [isEmailError, isNameError, newName, newEmail]);
 
   useEffect(() => {
     setNewEmail(currentUser.email)
@@ -38,14 +48,19 @@ export default function Profile(props) {
 
   const handleChangeEmail = (e) => {
     setNewEmail(e.target.value)
+    setEmailError(e.target.validity.valid);
+    console.log(`isFormValid =${isFormValid}`)
+    console.log(`isLading = ${props.isLoading}`)
+    console.log(`isValuesChanged = ${isValuesChanged}`)
   }
   const handleChangeName = (e) => {
     setNewName(e.target.value)
+    setIsNameError(e.target.validity.valid);
   }
-
   const [isValuesChanged, setIsValueChanged] = useState(false)
 
-
+  const isButtonDisabled = !(!isValuesChanged && isFormValid) || props.isLoading;
+  console.log(isButtonDisabled)
   return(
 
     <>
@@ -72,18 +87,19 @@ export default function Profile(props) {
           <div className="profile__place">
             <p className="profile__text">E-mail</p>
             <input
-              type="text"
+              type="email"
               className="profile__email profile__input"
               defaultValue={newEmail}
               disabled={inputsDisabled || props.isLoading}
               required
               onChange={handleChangeEmail}
+              pattern={emailPattern}
             />
           </div>
           <div className={`underline underline_grey ${underlineClass}`}></div>
           <p className="profile__message">{profileSavedMessage}</p>
           <button type="button" className={`profile__edit profile__button ${buttonSaveClass}`} onClick={handleEditButton}>Редактировать</button>
-          <button type="button" className={`profile__edit profile__button ${buttonEditClass}`} onClick={handleSaveButton} disabled={!isValuesChanged || props.isLoading}>Сохранить</button>
+          <button type="button" className={`profile__edit profile__button ${buttonEditClass}`} onClick={handleSaveButton} disabled={isButtonDisabled }>Сохранить</button>
           <button type="button" className="profile__signout" onClick={props.onLogout}>Выйти из аккаунта</button>
         </div>
         </section>
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -172,4 +172,4 @@ export const maxCardNumber = {
   mobileDeviceMaxCards: 5,
 }
 
-export const emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+export const emailPattern = `[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$`
